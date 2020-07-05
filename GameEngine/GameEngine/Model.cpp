1b95adfbc8a25ed2020de880793528cb86cb8409
jorge8199 <jorgesendarrubias@hotmail.com>
2020-07-05 14:28:06 +0200

    Add multiple source point lights!!

--- GameEngine/GameEngine/Model.cpp
+++ GameEngine/GameEngine/Model.cpp
@@ -45,8 +45,12 @@ Model::~Model()
 void Model::init()
 {
 	shader = new Shader(vertexLoc, fragmentLoc,
-		DirectionalLight(glm::vec3(1.0f, 1.0f, 1.0f), 0.2f, glm::vec3(0.0f, 0.0f, -1.0f), 1.2f),
-		SpecularLight(1.5f, 25.0f));
+		DirectionalLight(glm::vec3(1.0f, 1.0f, 1.0f), 0.2f, 0.2f, glm::vec3(0.0f, 0.0f, -1.0f)),
+		SpecularLight(0.5f, 25.0f));
+
+	shader->addPointLight(PointLight(glm::vec3(1.0f, 0.0f, 0.0f), 0.2f, 0.2f, glm::vec3(0.0f, 0.0f, -15.0f), 0.3f, 0.2f, 0.1f));
+	shader->addPointLight(PointLight(glm::vec3(0.0f, 1.0f, 0.0f), 0.2f, 0.2f, glm::vec3(10.0f, 0.0f, -15.0f), 0.3f, 0.2f, 0.1f));
+	shader->addPointLight(PointLight(glm::vec3(0.0f, 0.0f, 1.0f), 0.2f, 0.2f, glm::vec3(-10.0f, 0.0f, -15.0f), 0.3f, 0.2f, 0.1f));
 
 	model = glm::translate(glm::mat4(1.0f), position) * glm::scale(glm::mat4(1.0f), scale) /** glm::rotate(glm::mat4(1.0f), glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f))*/;
 
@@ -183,10 +187,11 @@ void Model::loadTextures(const aiScene* scene)
 			std::string textureFileName = location + realName;
 
 			textures[i] = new Texture(textureFileName);
+			/*textures[i] = new Texture("../Textures/white.png");*/
 		}
 		else
 		{
-			textures[i] = new Texture("../Textures/pjan.png");
+			textures[i] = new Texture("../Textures/white.png");
 		}
 	}
 }
@@ -197,12 +202,23 @@ void Model::uploadUniforms()
 	glUniformMatrix4fv(shader->getUniforms().viewLoc, 1, GL_FALSE, glm::value_ptr(camera->getViewMatrix()));
 	glUniformMatrix4fv(shader->getUniforms().projLoc, 1, GL_FALSE, glm::value_ptr(camera->getProjectionMatrix()));
 	glUniform3fv(shader->getUniforms().eyePositionLoc, 1, glm::value_ptr(camera->getPosition()));
+	glUniform1i(shader->getUniforms().numPointLightsLoc, shader->getPointLightUniforms().size());
 
 	glUniform3fv(shader->getLightUniforms().colourLoc, 1, glm::value_ptr(shader->getPhongLight()->getDirectionalLight().colour));
 	glUniform1f(shader->getLightUniforms().ambientIntensityLoc, shader->getPhongLight()->getDirectionalLight().ambientIntensity);
 	glUniform3fv(shader->getLightUniforms().directionLoc, 1, glm::value_ptr(shader->getPhongLight()->getDirectionalLight().direction));
 	glUniform1f(shader->getLightUniforms().diffuseIntensityLoc, shader->getPhongLight()->getDirectionalLight().diffuseIntensity);
-
 	glUniform1f(shader->getLightUniforms().specularIntensity, shader->getPhongLight()->getSpecularLight().specularIntensity);
 	glUniform1f(shader->getLightUniforms().specularPower, shader->getPhongLight()->getSpecularLight().specularPower);
+
+	for (int i = 0; i < shader->getPointLightUniforms().size(); i++)
+	{
+		glUniform3fv(shader->getPointLightUniforms()[i].colourLoc, 1, glm::value_ptr(shader->getPhongLight()->getPointLights()[i].colour));
+		glUniform1f(shader->getPointLightUniforms()[i].ambientIntensityLoc, shader->getPhongLight()->getPointLights()[i].ambientIntensity);
+		glUniform1f(shader->getPointLightUniforms()[i].diffuseIntensityLoc, shader->getPhongLight()->getPointLights()[i].diffuseIntensity);
+		glUniform3fv(shader->getPointLightUniforms()[i].positionLoc, 1, glm::value_ptr(shader->getPhongLight()->getPointLights()[i].position));
+		glUniform1f(shader->getPointLightUniforms()[i].constantLoc, shader->getPhongLight()->getPointLights()[i].constant);
+		glUniform1f(shader->getPointLightUniforms()[i].linearLoc, shader->getPhongLight()->getPointLights()[i].linear);
+		glUniform1f(shader->getPointLightUniforms()[i].expLoc, shader->getPhongLight()->getPointLights()[i].exp);
+	}
 }
